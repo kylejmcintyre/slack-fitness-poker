1329d186200bd5293f956f6efa88d838e3cfea4a
Kyle McIntyre <kyle.mcintyre@dev1.quiq.sh>
2022-09-26 17:45:34 +0000

    .

--- poker/engine.py
+++ poker/engine.py
@@ -166,6 +166,8 @@ def resend(slack, user_id, payload):
     with Connection() as conn:
         state = conn.load_game(payload['game_id'])
 
+        logger.info((state['handles'][state['current_player']], user_id))
+
         if state['handles'][state['current_player']] != user_id or state['status'] != 'in-progress':
             return
 
